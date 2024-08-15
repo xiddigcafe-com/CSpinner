9e0f3b407e07157f3bd92c4ed17f7cfecf92aa63
xiddigcafe.com <137005219+xiddigcafe-com@users.noreply.github.com>
2024-08-15 10:04:10 +0100

    Added the main file

---
+++ main.c
@@ -0,0 +1,28 @@
+//Written by A.Jama - Aug.2024
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+//Function to provide a small delay
+void delay(int numberOfSeconds){
+    int milliseconds = 1000 * numberOfSeconds;
+    clock_t start_time = clock();
+    while (clock() < start_time + milliseconds);
+}
+
+//Function to spin stuff
+void spinner(){
+    char spinnerPositions[4] = {'|','/','-','\\'};
+
+    int it0 = 0;
+    while (1){
+        printf("%c", spinnerPositions[it0]);
+        it0 = (it0 + 1) % strlen(spinnerPositions);
+        printf("\b");
+        delay(1);
+    }
+}
+
+int main(){
+    spinner();
+}
